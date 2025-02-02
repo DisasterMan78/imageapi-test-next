cc8e62efeee6a423a156cb4e4f14def8c59604be
David Benson <dmbenson1978@gmail.com>
2025-02-02 21:28:03 +0000

    TEST: ImageGrid - next button action (PASS)

--- __tests_/components/image-grid.test.tsx
+++ __tests_/components/image-grid.test.tsx
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom'
 import { render, screen, within } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import ImageGrid from '../../src/app/components/image-grid';
 import testData from '../image-test-data';
@@ -9,6 +10,7 @@ const imageGridProps = {
   thumbnailWidth: 300,
   thumbnailHeight: 200,
   page: 1,
+  onNavClick: jest.fn(() => {}),
 }
 
 describe('ImageGrid', () => {
@@ -39,5 +41,16 @@ describe('ImageGrid', () => {
 
     expect(buttons.length).toEqual(1)
     expect(buttons[0]).toHaveTextContent("Next page");
+  })
+
+  it('should triggers API request for page 2 when "Next page" button is clicked', async () => {
+    const user = userEvent.setup()
+    render(<ImageGrid { ...imageGridProps} />)
+
+    const nextButton = screen.getByRole('button', { name: /Next page/ });
+    expect(nextButton.getAttribute('value')).toEqual('2')
+    await user.click(nextButton)
+
+    expect(imageGridProps.onNavClick).toHaveBeenCalled()
   })
 })
