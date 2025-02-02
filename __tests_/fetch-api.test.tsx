663a25289d393587fb25e934283edde7eb3f1070
David Benson <dmbenson1978@gmail.com>
2025-02-02 12:39:32 +0000

    TEST: Implement MSW for mocking API fetch tests (FAIL)
    Signed-off-by: David Benson <dmbenson1978@gmail.com>

--- __tests_/fetch-api.test.tsx
+++ __tests_/fetch-api.test.tsx
@@ -1,9 +1,20 @@
+import {http, HttpResponse} from 'msw'
+import {setupServer} from 'msw/node'
 import '@testing-library/jest-dom'
 
 import FetchApiOnClient from '../src/app/fetch-api';
 
 const testApiURL = 'http://fake.api/test';
 const testResponse = { someKey: 'Some string data' }
+const server = setupServer(
+  http.get(testApiURL, () => {
+    return HttpResponse.json(testResponse)
+  }),
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
 
 describe('api fetch tests', () => {
   it('receives data from API on success', async () => {
