43bbc0d2d600c23ca332e5e7e8ec715087d3c94e
David Benson <dmbenson1978@gmail.com>
2025-02-02 18:06:40 +0000

    TEST: “Next page” button (FAIL)

--- __tests_/components/image-grid.test.tsx
+++ __tests_/components/image-grid.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import { render, screen, within } from '@testing-library/react'
 
 import ImageGrid from '../../src/app/components/image-grid';
 import testData from '../image-test-data';
@@ -8,6 +8,7 @@ const imageGridProps = {
   imageData: testData,
   thumbnailWidth: 300,
   thumbnailHeight: 200,
+  page: 1,
 }
 
 describe('ImageGrid', () => {
@@ -19,7 +20,6 @@ describe('ImageGrid', () => {
     expect(images.length).toEqual(testData.length)
   })
 
-
   it('displays author name for each image', () => {
     render(<ImageGrid { ...imageGridProps} />)
 
@@ -29,5 +29,14 @@ describe('ImageGrid', () => {
       expect(item).toHaveTextContent(testData[index].author)
     })
 
+  })
+
+  it('displays onluy a "Next page" button on page 1', () => {
+    render(<ImageGrid { ...imageGridProps} />)
+
+    const nav = screen.getByRole('navigation');
+    const buttons = within(nav).getAllByRole('button')
+
+    expect(buttons.length).toEqual(1)
   })
 })
