80b604460023540a40467f04a2d18d24ed62fc29
David Benson <dmbenson1978@gmail.com>
2025-02-02 18:06:59 +0000

    CODE: “Next page” button (PASS)

--- src/app/components/image-grid.tsx
+++ src/app/components/image-grid.tsx
@@ -15,29 +15,40 @@ export type ImageGridProps = {
   imageData: PicsumImage[],
   thumbnailWidth: number,
   thumbnailHeight: number,
+  page: number,
 }
+
+
+const Navigation = () => (
+  <div role="navigation">
+    <button>Next page</button>
+  </div>
+)
 
 const ImageGrid = ({imageData, thumbnailWidth, thumbnailHeight}: ImageGridProps) => {
   const thumbnailURL = (url: string) => url.replace(/\d*\/\d*$/, `${thumbnailWidth}/${thumbnailHeight}`);
 
   return (
-    <ul className={styles.thumbnailGrid} data-testid="image-grid">
-      {
-        imageData.map(image => (
-          <li key={image.id}>
-            <figure>
-              <Image
-                width={thumbnailWidth}
-                height={thumbnailHeight}
-                src={thumbnailURL(image.download_url)}
-                alt={`Image by ${image.author}`}
-              />
-              <figcaption>{image.author}</figcaption>
-            </figure>
-          </li>
-        ))
-      }
-    </ul>
+    <div data-testid="image-grid">
+      { Navigation() }
+      <ul className={styles.thumbnailGrid}>
+        {
+          imageData.map(image => (
+            <li key={image.id}>
+              <figure>
+                <Image
+                  width={thumbnailWidth}
+                  height={thumbnailHeight}
+                  src={thumbnailURL(image.download_url)}
+                  alt={`Image by ${image.author}`}
+                />
+                <figcaption>{image.author}</figcaption>
+              </figure>
+            </li>
+          ))
+        }
+      </ul>
+    </div>
   );
 }
 
