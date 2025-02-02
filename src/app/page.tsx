a799ffc5561c7d27665e1eee178fbb64a4f6d45b
David Benson <dmbenson1978@gmail.com>
2025-02-02 14:17:38 +0000

    CODE: Fetch picsum data, display raw result (FAIL)
    Signed-off-by: David Benson <dmbenson1978@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
+"use client"
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
+import FetchApiOnClient from "./fetch-api";
 
- const Home = () => (
+const Home = () => {
+  const [images, setImages] = useState(null);
+
+  useEffect(() => {
+    FetchApiOnClient('https://picsum.photos/v2/list')
+      .then(response => {
+        setImages(response);
+      })
+  }, []);
+
+  return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h1 role="heading" aria-level={1}>Snowplow test - Picsum API</h1>
+        <pre data-testid="picsum-result">{JSON.stringify(images, null, 2)}</pre>
       </main>
     </div>
   );
+}
 
 export default Home;
