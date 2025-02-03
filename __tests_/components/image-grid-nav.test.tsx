a734e9adde58582d936ed526de535e1e761f610c
David Benson <dmbenson1978@gmail.com>
2025-02-03 11:10:48 +0000

    TEST: Update image-grid tests to test image button launching edit image view

---
+++ __tests_/components/image-grid-nav.test.tsx
@@ -0,0 +1,86 @@
+import '@testing-library/jest-dom'
+import { render, screen, within } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import ImageGrid from '../../src/app/components/image-grid'
+import testData from '../image-test-data'
+
+const imageGridProps = {
+  imageData: testData,
+  APILimit: 4,
+  thumbnailWidth: 300,
+  thumbnailHeight: 200,
+  page: 1,
+  onNavClick: jest.fn(() => { }),
+  onImageClick: jest.fn(() => {}),
+}
+
+// This file split out from the other image-grid tests as a quick
+// fix for `onNavClick` call test failing when `onImageClick` is
+// called in an earlier test. Jest mocks just need resetting properly,
+// but this means I'm not held up fixing it. Minor tech-debt, arguably
+
+describe('ImageGrid', () => {
+
+  it('displays only a "Next page" button on page 1', () => {
+    render(<ImageGrid { ...imageGridProps} />)
+
+    const nav = screen.getAllByRole('navigation')[0];
+    const buttons = within(nav).getAllByRole('button')
+
+    expect(buttons.length).toEqual(1)
+    expect(buttons[0]).toHaveTextContent('Next page')
+  })
+
+  it('displays both buttons on intermediate page', () => {
+    render(<ImageGrid {...{
+      ...imageGridProps,
+      page: 2,
+      APILimit: 30
+    }} />)
+
+    const nav = screen.getAllByRole('navigation')[0];
+    const buttons = within(nav).getAllByRole('button')
+
+    expect(buttons.length).toEqual(2)
+    expect(buttons[0]).toHaveTextContent('Prev page')
+    expect(buttons[1]).toHaveTextContent('Next page')
+  })
+
+  it('displays only a "Prev page" button on last page', () => {
+    render(<ImageGrid {...{
+      ...imageGridProps,
+      page: 34,
+      APILimit: 30,
+      imageData: testData.slice(0, 4),
+    }} />)
+
+    const nav = screen.getAllByRole('navigation')[0];
+    const buttons = within(nav).getAllByRole('button')
+
+    expect(buttons.length).toEqual(1)
+    expect(buttons[0]).toHaveTextContent('Prev page')
+  })
+
+  it('should trigger API request for page 2 when "Next page" button is clicked', async () => {
+    const user = userEvent.setup()
+    render(<ImageGrid { ...imageGridProps} />)
+
+    const nextButton = screen.getAllByRole('button', { name: /Next page/ })[0];
+    expect(nextButton.getAttribute('value')).toEqual('2')
+    await user.click(nextButton)
+
+    expect(imageGridProps.onNavClick).toHaveBeenCalled()
+  })
+
+  it('should trigger API request for page 1 when "Prev page" button is clicked on page 2', async () => {
+    const user = userEvent.setup()
+    render(<ImageGrid {...{ ...imageGridProps, page: 2}} />)
+
+    const nextButton = screen.getAllByRole('button', { name: /Prev page/ })[0];
+    expect(nextButton.getAttribute('value')).toEqual('1')
+    await user.click(nextButton)
+
+    expect(imageGridProps.onNavClick).toHaveBeenCalled()
+  })
+})
--- __tests_/components/image-grid.test.tsx
+++ __tests_/components/image-grid.test.tsx
@@ -15,69 +15,11 @@ const imageGridProps = {
   onImageClick: jest.fn(() => {}),
 }
 
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
 describe('ImageGrid', () => {
-
-  it('displays one image for each item in the API data', () => {
-    render(<ImageGrid { ...imageGridProps} />)
-    const images = screen.getAllByRole('img')
-
-    expect(images.length).toEqual(testData.length)
-  })
-
-  it('displays author name for each image', () => {
-    render(<ImageGrid {...{
-      ...imageGridProps,
-      // Limit data length to make test faster
-      imageData: testData.slice(0, 4),
-    }} />)
-
-    const items = screen.getAllByRole('listitem')
-
-    items.forEach((item, index) => {
-      expect(item).toHaveTextContent(testData[index].author)
-    })
-
-  })
-
-  it('displays only a "Next page" button on page 1', () => {
-    render(<ImageGrid { ...imageGridProps} />)
-
-    const nav = screen.getAllByRole('navigation')[0];
-    const buttons = within(nav).getAllByRole('button')
-
-    expect(buttons.length).toEqual(1)
-    expect(buttons[0]).toHaveTextContent('Next page')
-  })
-
-  it('displays both buttons on intermediate page', () => {
-    render(<ImageGrid {...{
-      ...imageGridProps,
-      page: 2,
-      APILimit: 30
-    }} />)
-
-    const nav = screen.getAllByRole('navigation')[0];
-    const buttons = within(nav).getAllByRole('button')
-
-    expect(buttons.length).toEqual(2)
-    expect(buttons[0]).toHaveTextContent('Prev page')
-    expect(buttons[1]).toHaveTextContent('Next page')
-  })
-
-  it('displays only a "Prev page" button on last page', () => {
-    render(<ImageGrid {...{
-      ...imageGridProps,
-      page: 34,
-      APILimit: 30,
-      imageData: testData.slice(0, 4),
-    }} />)
-
-    const nav = screen.getAllByRole('navigation')[0];
-    const buttons = within(nav).getAllByRole('button')
-
-    expect(buttons.length).toEqual(1)
-    expect(buttons[0]).toHaveTextContent('Prev page')
-  })
 
   it('shows a notice if no image data is received', () => {
     render(<ImageGrid {...{
@@ -90,14 +32,35 @@ describe('ImageGrid', () => {
     expect(notice).toHaveTextContent('No more images to display')
   })
 
-  it('should triggers API request for page 2 when "Next page" button is clicked', async () => {
-    const user = userEvent.setup()
+  it('displays one image for each item in the API data', () => {
     render(<ImageGrid { ...imageGridProps} />)
+    const images = screen.getAllByRole('img')
 
-    const nextButton = screen.getAllByRole('button', { name: /Next page/ })[0];
-    expect(nextButton.getAttribute('value')).toEqual('2')
-    await user.click(nextButton)
+    expect(images.length).toEqual(testData.length)
+  })
 
-    expect(imageGridProps.onNavClick).toHaveBeenCalled()
+  it('has a button that triggers edit image page, and displays author name for each image', () => {
+    const user = userEvent.setup()
+
+    render(<ImageGrid {...{
+      ...imageGridProps,
+      // Limit data length to make test faster
+      imageData: testData.slice(0, 4),
+    }} />)
+
+    const items = screen.getAllByRole('listitem')
+
+    items.forEach(async (item, index) => {
+      const button = within(item).getByRole('button')
+      // console.log(button)
+      expect(button).toBeInTheDocument()
+      expect(button.getAttribute('value')).toEqual(testData[index].id)
+      await user.click(button)
+
+      expect(imageGridProps.onImageClick).toHaveBeenCalled()
+
+      expect(item).toHaveTextContent(testData[index].author)
+    })
+
   })
 })
