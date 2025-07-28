d1b2fca73315a46658c20bc777d56e994327f93f
David Benson <dmbenson1978@gmail.com>
2025-07-29 00:15:02 +0100

    TEST: Basic blur - update test results

--- __tests_/utils/basic-blur.test.ts
+++ __tests_/utils/basic-blur.test.ts
@@ -19,22 +19,22 @@ describe('basic blur tests', () => {
   it('can calculate the average value of each colour channel from the 8 pixels around a given pixel in some image data', async () => {
     const pixelMatrix = imageDataToPixelMatrix(rawImageData);
     const red = pixelMatrix[1][1][0];
-    const averageNeighbourRed = averageNeighbourByChannel(pixelMatrix, 1, 1, 0, {})
+    const averageNeighbourRed = averageNeighbourByChannel(pixelMatrix, 1, 1, 0, 1)
 
     const green = pixelMatrix[1][1][1];
-    const averageNeighbourGreen = averageNeighbourByChannel(pixelMatrix, 1, 1, 1, {})
+    const averageNeighbourGreen = averageNeighbourByChannel(pixelMatrix, 1, 1, 1, 1)
 
     const blue = pixelMatrix[1][1][2];
-    const averageNeighbourBlue = averageNeighbourByChannel(pixelMatrix, 1, 1, 2, {})
+    const averageNeighbourBlue = averageNeighbourByChannel(pixelMatrix, 1, 1, 2, 1)
 
     expect(averageNeighbourRed).not.toEqual(red)
-    expect(averageNeighbourRed).toEqual(198)
+    expect(averageNeighbourRed).toEqual(234)
 
     expect(averageNeighbourGreen).not.toEqual(green)
-    expect(averageNeighbourGreen).toEqual(56)
+    expect(averageNeighbourGreen).toEqual(85)
 
     expect(averageNeighbourBlue).not.toEqual(blue)
-    expect(averageNeighbourBlue).toEqual(85)
+    expect(averageNeighbourBlue).toEqual(43)
   })
 
 
@@ -45,9 +45,9 @@ describe('basic blur tests', () => {
     expect(blurredImageData).not.toEqual(dataInDecimal);
 
     expect(blurredImageData).toEqual(new Uint8ClampedArray([
+      255, 64, 0, 255, 255, 127, 0, 255, 255, 191, 0, 255,
       223, 32, 64, 255, 234, 85, 43, 255, 255, 127, 0, 255,
       170, 21, 127, 255, 198, 56, 85, 255, 234, 85, 43, 255,
-      128, 0, 191, 255, 170, 21, 127, 255, 223, 32, 64, 255,
     ]))
   })
 })
